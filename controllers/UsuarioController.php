858340c346339b120f701f0f256c1cce35153742
danielaorellana2992-rgb <daniela.orellana2992@gmail.com>
2026-04-26 13:28:44 -0400

    Modificacion leve

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -5,13 +5,13 @@ require_once '../models/Usuario.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $usuario = new Usuario($conexion);
     if ($usuario->registrar($_POST['nombre'], $_POST['email'], $_POST['password'])) {
-        // Al darle "Aceptar", window.location.href te enviará al index
+        
         echo "<script>
                 alert('¡Registro exitoso!'); 
                 window.location.href='../views/index.php';
               </script>";
     } else {
-        // Por si algo falla, te avisa y te deja en el registro
+        
         echo "<script>
                 alert('Error al registrar. Inténtalo de nuevo.'); 
                 window.location.href='../views/registro.php';
